84a9824629f68203750ea057e0d688b4efd10002
sergiopm97 <sergio.pm.97@protonmail.com>
2022-12-08 17:04:01 +0100

    [ORB-01] Obtain origin data
    Obtaining historical soccer match data from the origin database

--- engines/collector_engine.py
+++ engines/collector_engine.py
@@ -1,3 +1,24 @@
+import ssl
+
+import pandas as pd
+
+
 class CollectorEngine:
     def __init__(self, origin_database_url: str) -> None:
         self.origin_database_url = origin_database_url
+
+    def get_data(self) -> pd.DataFrame:
+        """
+        Obtain historical soccer match data
+        from FiveThirtyEight's origin database
+
+
+        Returns:
+            pd.DataFrame:
+                DataFrame with historical
+                data on soccer matches
+        """
+
+        ssl._create_default_https_context = ssl._create_unverified_context
+
+        return pd.read_csv(self.origin_database_url)
--- scripts/collector.py
+++ scripts/collector.py
@@ -1,6 +1,6 @@
-from engines.collector_engine import CollectorEngine
 import configparser
 
+from engines.collector_engine import CollectorEngine
 
 if __name__ == "__main__":
 
@@ -11,4 +11,4 @@ if __name__ == "__main__":
         config["FIVETHIRTYEIGHT"]["database_url"]
     )
 
-    print()
+    soccer_matches_df = collector_engine.get_data()
